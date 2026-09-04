86172d1d14948050cbb557a5d74e15d77d94bc5d
Tony Huynh <tonyh.projectblue@gmail.com>
2026-09-04 13:52:52 -0500

    Removed redudant bridge exposure

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -64,25 +64,6 @@ const api = {
   }
 }
 
-declare global {
-  interface Window {
-    electron: any
-    api: any
-  }
-}
-
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
-} else {
-  window.electron = electronAPI
-  window.api = api
-}
-
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
